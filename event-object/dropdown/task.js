f68381facf00ed33ee4e79a036f12f60c9466d8b
Malays <malays92@mail.ru>
2024-02-04 15:54:54 +0300

    Задание выполнено.Fix01

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,27 +1,14 @@
-var dropdowns = document.querySelectorAll('.dropdown');
+const value = document.querySelector('.dropdown__value');
+const list = document.querySelector('.dropdown__list');
+const items = list.querySelectorAll('.dropdown__item');
 
-// Обходим все найденные кнопки-выпадающие списки
-dropdowns.forEach(function(dropdown) {
-    // Находим в каждой кнопке значение и список
-    var value = dropdown.querySelector('.dropdown__value');
-    var list = dropdown.querySelector('.dropdown__list');
-
-    // Добавляем обработчик события клика по значению кнопки
-    value.addEventListener('click', function() {
-        // Сворачиваем/разворачиваем список при клике на значение кнопки
-        list.classList.toggle('dropdown__list_active');
-    });
-
-    // Добавляем обработчик события клика по пунктам меню
-    var items = list.querySelectorAll('.dropdown__item');
-    items.forEach(function(item) {
-        item.addEventListener('click', function(event) {
-            // Предотвращаем переход по ссылке при клике на пункт меню
-            event.preventDefault();
-            // Меняем значение кнопки на выбранное значение пункта меню
-            value.textContent = item.textContent.trim();
-            // Скрываем список после выбора значения
-            list.classList.remove('dropdown__list_active');
-        });
-    });
+value.addEventListener('click', () => {
+  list.classList.toggle('dropdown__list_active');
+});
+items.forEach(item => {
+  item.addEventListener('click', event => {
+    event.preventDefault();
+    value.textContent = item.textContent.trim();
+    list.classList.remove('dropdown__list_active');
+  });
 });
