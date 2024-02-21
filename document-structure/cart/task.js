7147905e4d5ca040eb0fcf01c657d56015979af7
Malays <malays92@mail.ru>
2024-02-21 18:26:50 +0300

    Задание исправлено

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const products = document.querySelectorAll('.product');
-    const cart = document.querySelector('.cart');
+    const cart = document.querySelector('.cart__products');
 
     products.forEach(product => {
         const productId = product.getAttribute('data-id');
         const productImage = product.querySelector('.product__image').getAttribute('src');
         const quantityValue = product.querySelector('.product__quantity-value');
+        const addToCartBtn = product.querySelector('.product__add');
 
         const addToCart = () => {
             const existingCartItem = cart.querySelector(`.cart__product[data-id="${productId}"]`);
@@ -20,20 +21,20 @@ document.addEventListener('DOMContentLoaded', function() {
                     <img class="cart__product-image" src="${productImage}">
                     <div class="cart__product-count">${quantityValue.textContent}</div>
                 `;
-                cart.appendChild(cartProduct);
+                cart.append(cartProduct);
             }
         };
 
         product.querySelector('.product__quantity-control_dec').addEventListener('click', () => {
             if (parseInt(quantityValue.textContent) > 1) {
-                quantityValue.textContent = parseInt(quantityValue.textContent) - 1;
+                quantityValue.textContent--;
             }
         });
 
         product.querySelector('.product__quantity-control_inc').addEventListener('click', () => {
-            quantityValue.textContent = parseInt(quantityValue.textContent) + 1;
+            quantityValue.textContent++;
         });
 
-        product.addEventListener('click', addToCart);
+        addToCartBtn.addEventListener('click', addToCart);
     });
 });
